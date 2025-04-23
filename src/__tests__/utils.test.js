0e05b89f11e2e913b55bcc64bafd3795657cdf6a
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-23 19:02:07 +0300

    completes test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,18 @@
 // Your tests here
+import {pointsForWord} from "../utils"
+
+describe("pointsForWord", ()=>{
+    test("calculates the total points for a waord (1 point per vowel, 2 per consonant)",()=>{
+        const word = "test"
+        const result = pointsForWord(word);
+        expect(result).toBe(7);
+    })
+    
+    it("handles uppercase and lowercase input", () => {
+        const word = "tEsT";
+    
+        const points = pointsForWord(word);
+    
+        expect(points).toBe(7);
+    });
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function pointsForWord(word){
+    let points = 0;
+    if (!word){
+        return points
+    }
+    for(let letter of word){
+        points += /[aeiou]/.test(letter.toLowerCase()) ? 1 : 2;
+    }
+    return points
+}
+
+pointsForWord("test")
